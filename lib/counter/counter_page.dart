2015a2660c48da52fb54f34e57548bc25a675193
Christof Kost <christof.kost@mway.io>
2021-05-25 11:10:46 +0200

    increment with pure int works

--- lib/counter/counter_page.dart
+++ lib/counter/counter_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_notifier_counter/counter/counter_notifier.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -18,15 +20,21 @@ class MyHomePage extends StatelessWidget {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Consumer(builder:
+                (BuildContext context, ScopedReader watch, Widget child) {
+              final incrementsState = watch(counterStateNotifierProvider);
+              return Text(
+                '$incrementsState',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => context
+            .read(counterStateNotifierProvider.notifier)
+            .incrementCounter(),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
